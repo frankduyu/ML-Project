0d8a02103b291e483ac005682c24dfe008658740
yudu <yu.du@brainco.tech>
2018-08-06 15:09:23 -0400

    adding sliding window

--- LBPH.py
+++ LBPH.py
@@ -6,16 +6,25 @@ Part 1: Image Processing to Pattern through LBPH Algorithm.
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.patches as patches
+from PIL import Image
 import numpy as np
+import pickle
 
 
 class Image:
     def __init__(self):
         self.IMG_LEN = 0
         self.IMG_WID = 0
+
         self.image = []
         self.pattern = []
         self.img_lbp = []
+
+        self.para_dict = pickle.load(open('face_detection_parameter.pickle', 'rb'))
+        self.train_patterns = self.para_dict['train_data_pattern']
+        self.dist_bar = self.para_dict['dist_bar']
+        self.prob_bar = self.para_dict['proba_bar']
 
     def _img_show(self):
         plt.imshow(self.img_lbp, cmap=plt.get_cmap('gray'))
@@ -42,30 +51,92 @@ class Image:
         self.img_lbp = np.reshape(img_lbp_1d, (int(len(img_lbp_1d) / self.IMG_WID), self.IMG_WID))
         # self._img_show()
 
-    def _hist_pattern(self, k=4):
-        sep_len = (self.IMG_LEN-2)//k
-        sep_wid = self.IMG_WID//k
+    def _dist(self, patt1, patt2):
+        diff = np.array(patt1)-np.array(patt2)
+        distance = sum(diff**2)
+        return distance
+
+    def _face_detect(self, pattern):
+        test_dist = []
+        for patt in self.train_patterns[:100]:
+            distance = self._dist(pattern, patt)
+            test_dist.append(distance)
+
+        # calculate test data probability
+        test_prob = sum(dist <= self.dist_bar for dist in test_dist) / len(test_dist)
+        print(test_prob)
+
+        if test_prob >= self.prob_bar:
+            print("This is a face")
+            return 1
+        else:
+            pass
+
+    def _slid_wind(self, interval=4, win_end=24):
+        img_lbp = self.img_lbp
+        win_len = len(img_lbp)
+        win_wid = len(img_lbp[0])
+        win_side = min(win_wid, win_len)
+        moving_step = win_side // interval
+        crops = []
+
+        if self._face_detect(self._hist_pattern(img_lbp)) == 1:
+            crops.append([0, 0, win_len, win_wid])
+
+        while win_side >= win_end:
+            print('start')
+            for i in range(0, int(win_len - win_side), moving_step):
+                for j in range(0, int(win_wid - win_side), moving_step):
+                    win_data = img_lbp[i: int(i + win_side), j: int(j + win_side)]
+                    filter_patt = self._hist_pattern(win_data)
+                    if self._face_detect(filter_patt) == 1:
+                        crops.append([i, j, win_side, win_side])
+                        # img_lbp[i: int(i + win_side), j: int(j + win_side)] = 0
+
+            win_side = win_side * interval / (interval + 1)
+            moving_step = int(win_side // interval)
+
+        return crops
+
+    def _normalize(self, face_candidate):
+
+        face_candidate = np.array(face_candidate)
+        sum_item = np.sum(face_candidate)
+        face_candidate = face_candidate / sum_item
+
+        return face_candidate
+
+    def _hist_pattern(self, img_lbp, k=4):
+        win_len = len(img_lbp)
+        win_wid = len(img_lbp[0])
+        sep_len = win_len//k
+        sep_wid = win_wid//k
         hist = np.zeros(256*(k**2))
 
         for i in range(sep_len*k):
             for j in range(sep_wid*k):
                 index = (j // sep_wid) + (i // sep_len) * k
-                index = index * 256 + int(self.img_lbp[i][j])
+                index = index * 256 + int(img_lbp[i][j])
                 hist[index] = hist[index] + 1
-        self.pattern = hist
+        hist = self._normalize(hist)
+        return hist
 
-    def _normalize(self):
-        self.pattern = np.array(self.pattern)
-        sum_item = np.sum(self.pattern)
-        self.pattern = self.pattern / sum_item
+    def _draw_crops(self, raw_image, crops):
+        im = self.image
+        fig, ax = plt.subplots(1)
+        ax.imshow(im)
+        for item in crops:
+            rect = patches.Rectangle((item[0], item[1]), item[2], item[3], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+        plt.show()
 
     def pattern_return(self, img_name):
         # parameter initial
-        self.image = mpimg.imread(img_name)
-        self.image = np.dot(self.image[..., :3], [0.2989, 0.5870, 0.1140])
+        color_image = mpimg.imread(img_name)
+        self.image = np.dot(color_image[..., :3], [0.2989, 0.5870, 0.1140])
         self.IMG_LEN = len(self.image)
         self.IMG_WID = len(self.image[0])
         self._img_pre()
-        self._hist_pattern()
-        self._normalize()
-        return self.pattern
+        crops = self._slid_wind()
+        if crops != []:
+            self._draw_crops(img_name, crops)
--- face_detection.py
+++ face_detection.py
@@ -5,10 +5,7 @@ import pickle
 class FaceDetect:
     def __init__(self, img):
         # parameter initialize
-        self.para_dict = pickle.load(open('face_detection_parameter.pickle', 'rb'))
-        self.train_patterns = self.para_dict['train_data_pattern']
-        self.dist_bar = self.para_dict['dist_bar']
-        self.prob_bar = 0.8
+
         self.img = img
 
     def _dist(self, patt1, patt2):
@@ -17,18 +14,21 @@ class FaceDetect:
         return distance
 
     def face_detect(self, img_path):
-        test_dist = []
-        test_data = self.img.pattern_return(img_path)
 
-        for patt in self.train_patterns:
-            distance = self._dist(test_data, patt)
-            test_dist.append(distance)
+        test_patterns = self.img.pattern_return(img_path)
 
-        # calculate test data probability
-        test_prob = sum(dist <= self.dist_bar for dist in test_dist) / len(test_dist)
-        print(test_prob)
+        for pattern in test_patterns:
+            test_dist = []
+            for patt in self.train_patterns[:100]:
+                distance = self._dist(pattern, patt)
+                test_dist.append(distance)
 
-        if test_prob >= self.prob_bar:
-            print("This is a face")
-        else:
-            print("This is NOT a face")
+            # calculate test data probability
+            test_prob = sum(dist <= self.dist_bar for dist in test_dist) / len(test_dist)
+            print(test_prob)
+
+            if test_prob >= self.prob_bar:
+                print("This is a face")
+            else:
+                # print("This is NOT a face")
+                pass
